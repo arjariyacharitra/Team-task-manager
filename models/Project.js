dec863c7950f700d921a14e047c936038dcaa659
arjariyacharitra <112388716+arjariyacharitra@users.noreply.github.com>
2026-05-01 11:50:32 +0530

    Delete models directory

--- models/Project.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const schema = new mongoose.Schema({
-  name: String,
-  createdBy: String,
-  members: [String]
-});
-
-export default mongoose.models.Project || mongoose.model("Project", schema);
